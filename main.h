f9f36c7054b79b0058a0cd451d4056dc7bcb01a2
Faevoh <favourigbani29@gmail.com>
2023-11-07 06:35:24 -0800

    Added prototypes

--- main.h
+++ main.h
@@ -5,5 +5,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
+
+
+int access(const char *pathname, int mode);
+int chdir(const char *path);
+int execve(const char *pathname, char *const _Nullable argv[], char *const _Nullable envp[]);
+[[noreturn]] void exit(int status);
+pid_t fork(void);
+char *getcwd(char buf[.size], size_t size);
+ssize_t getline(char **restrict lineptr, size_t *restrict n, FILE *restrict stream);
+pid_t getpid(void);
+pid_t wait(int *_Nullable wstatus);
 
 #endif
